40da83a09ae779da336c8d18915e6c3c77ab6088
vamshi79957 <vamshikrishnalode94@gmail.com>
2026-09-22 19:14:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// ========================================
+// STUDENT LOGIN BUTTON
+// ========================================
+
+function loginMessage() {
+
+    alert("Student Login page will be available soon!");
+
+}
+
+
+
+// ========================================
+// SERVICE BUTTONS
+// ========================================
+
+function showMessage(serviceName) {
+
+    alert(serviceName + " section will be available soon!");
+
+}
